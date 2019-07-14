4c32cb7d1cc2c94f29b8bca4414f7dee804de221
ChristopherJamesN <nady.christopher@gmail.com>
2019-07-14 13:41:29 -0500

    Update dependencies.

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <WebView
+          source={{uri: 'https://katy-hiking-trails.herokuapp.com/'}}
+          style={{marginTop: 20}}
+        />
       </View>
     );
   }
